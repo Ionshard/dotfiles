d21e83b215b41fca03e5b071e53dccd23ffdc15c
Corey Ling <corey.ling@cenx.com>
2016-04-07 14:51:10 -0400

    Add .lein/profiles.clj

---
+++ lein/profiles.clj
@@ -0,0 +1,3 @@
+{:user {:dependencies [[pjstadig/humane-test-output "0.7.1"]]
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]}}

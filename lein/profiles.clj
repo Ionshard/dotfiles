fc96285e28d9d2601933be91132f04de30411b01
Corey Ling <corey.ling@cenx.com>
2017-01-12 15:22:27 -0500

    Remove profiles stuff. CIDER handles it

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,3 @@
 {:user {:dependencies [[pjstadig/humane-test-output "0.7.1"]]
         :injections [(require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)]}
- :plugins [[refactor-nrepl "2.2.0"]
-           [cider/cider-nrepl "0.11.0"]]}
+                     (pjstadig.humane-test-output/activate!)]}}

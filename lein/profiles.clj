a26b3413e7641b3803c48b3bc59bde2f641ddbd6
Corey Ling <corey.ling@cenx.com>
2017-02-06 14:31:00 -0500

    Remove old version of humane-test-output

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,1 @@
-{:user {:dependencies [[pjstadig/humane-test-output "0.7.1"]]
-        :injections [(require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)]}}
+{}
